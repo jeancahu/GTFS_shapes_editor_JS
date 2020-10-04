6486b053153199489ca3bc3fe770c801c7911624
Jeancarlo Hidalgo <jeancahu@gmail.com>
2020-10-04 03:02:15 -0600

    add some decorations

---
+++ README.md
@@ -0,0 +1,11 @@
+# Experiments with npm:
+
+    $ npm install . # add package dependencies locally into node_modules directory
+	$ npm run serve # a small web-server using express form node.js
+
+
+
+(index)[http://localhost/]<br>
+(A form)[http://localhost/form]<br>
+http://localhost:9000/ejemplo?name=variable_content<br>
+http://localhost:9000/ejemplo/vArIaBlE_cOnTeNt<br>
--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
+const port = 9000;
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Este es el resultado a pedir la dirección /
 app.get('/', (request, response) => response.send("Hello World!"));
 
+app.get('/form', (request, response) => {
+    response.sendFile( __dirname + "/templates/" + "form.html" );
+})
+
+app.get('/vue_js', (request, response) => {
+    response.sendFile( __dirname + "/templates/" + "vue_js.html" );
+})
+
+
 app.get('/ejemplo', (request, response) => {
 
-    // http://localhost:9000/ejemplo?name=algo
+    // http://localhost:$port/ejemplo?name=algo
     const { name } = request.query;
     response.send(`Hello ${name || 'Anon'}!`);
 })
 
 app.get('/ejemplo/:name', (request, response) => {
 
-    // http://localhost:9000/ejemplo?name=algo
+    // http://localhost:$port/ejemplo?name=algo
     const { name } = request.params;
     response.send(`Hello ${name || 'Anon'}!`);
 })
 
+app.get('/process_get', (request, response) => {
+    json_response = {
+        first_name:request.query.first_name_get,
+        last_name:request.query.last_name_get
+    };
+    // This is what we receive
+    console.log(json_response);
+    response.end(JSON.stringify(json_response));
+})
+
+// Use the application urlencodedParser
+app.post('/process_post', urlencodedParser, (request, response) => {
+    json_response = {
+        first_name:request.body.first_name_post,
+        last_name:request.body.last_name_post
+    };
+    // This is what we receive
+    console.log(json_response);
+    response.end(JSON.stringify(json_response));
+})
+
+
+// It serves static files
+app.use(express.static('static'));
+
 // Iniciamos el servidor
-console.log("Servidor en http://localhost:9000\n");
-app.listen(9000);
+console.log(`Servidor en http://localhost:${port}\n`);
+app.listen(port);
